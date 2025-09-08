fdabc5332310a19b507c14c8cf9ea36b411ef5e7
Bhargav <vyasb1503@gmail.com>
2025-09-08 12:55:04 +0530

    this is  the  change  in the  footeer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import {
+  FaLinkedin,
+  FaTwitter,
+  FaGithub,
+  FaInstagram,
+  FaEnvelope,
+} from "react-icons/fa";
+import "./Footer.css";
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3">
+    <footer className="custom-footer text-white py-4">
       <Container>
-        <p>&copy; 2025 My Portfolio</p>
+
+        {/* Top: Portfolio Text */}
+        <div className="footer-center-top">
+          <p>
+            © 2025 <span>My Portfolio</span> | Designed by <strong>Bhargav Vyas</strong>
+          </p>
+        </div>
+
+        {/* Bottom: Links Left + Right */}
+        <div className="footer-row">
+          {/* Left side */}
+          <div className="footer-left">
+            <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin /> LinkedIn
+            </a>
+            <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaTwitter /> Twitter
+            </a>
+          </div>
+
+          {/* Right side */}
+          <div className="footer-right">
+            <a href="https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaInstagram /> Instagram
+            </a>
+            <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaGithub /> GitHub
+            </a>
+            <a href="mailto:youremail@example.com">
+              <FaEnvelope /> Contact
+            </a>
+          </div>
+        </div>
+
       </Container>
     </footer>
   );
